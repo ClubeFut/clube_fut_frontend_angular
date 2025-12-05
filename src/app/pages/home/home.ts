28f664d6d94e4473949ce363777382c5d748bc9b
Namibo06 <namibo654@gmail.com>
2025-12-04 22:51:12 -0300

    feta: criada uma parte das estatisticas

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Header } from '../../shared/header/header';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    Header,
+    Footer
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
