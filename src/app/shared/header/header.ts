85cf427ba068f6bc94ac9b3adafd625c64cf8c64
Namibo06 <namibo654@gmail.com>
2025-12-04 22:50:21 -0300

    feat: header para tela de home

---
+++ src/app/shared/header/header.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    CommonModule,
+    
+  ],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {
+
+}
