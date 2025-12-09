65b20611afde45dfa46857e1b522b835a4838b1f
Namibo06 <namibo654@gmail.com>
2025-12-08 23:23:17 -0300

    chore: adicionando componente de perfil nas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Games } from './pages/games/games';
+import { Profile } from './pages/profile/profile';
 
 export const routes: Routes = [
     {
@@ -20,6 +21,10 @@ export const routes: Routes = [
     {
         path: "jogos",
         component: Games
+    },
+    {
+        path: "perfil",
+        component: Profile
     },
     {
         path: "**",
