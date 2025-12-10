c17054034327ec917a37539af4ce7dd0ac78e2fd
Namibo06 <namibo654@gmail.com>
2025-12-10 01:45:55 -0300

    feat: adicionando footer a tela de perfil

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,8 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    Footer
+  ],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
