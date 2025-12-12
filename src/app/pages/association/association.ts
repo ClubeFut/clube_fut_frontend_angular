1877bed6c81fe325c50f8840606000cf5f7f70fc
Namibo06 <namibo654@gmail.com>
2025-12-12 00:02:31 -0300

    feat: criada toda estrutura, e algumas partes com css

--- src/app/pages/association/association.ts
+++ src/app/pages/association/association.ts
@@ -1,8 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-association',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    Footer
+  ],
   templateUrl: './association.html',
   styleUrl: './association.scss',
 })
